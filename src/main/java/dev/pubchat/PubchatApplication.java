1c200bdcc7562e4e4c18574e914e426e026102ec
gustavo <geraldellig@gmail.com>
2025-08-08 12:33:45 -0300

    first commit

---
+++ src/main/java/dev/pubchat/PubchatApplication.java
@@ -0,0 +1,13 @@
+package dev.pubchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pubchat/PubchatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pubchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
