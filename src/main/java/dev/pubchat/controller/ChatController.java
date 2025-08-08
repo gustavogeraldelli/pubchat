1c3961197483d7eeffe0e3da1443c2d6b38b7006
gustavo <geraldellig@gmail.com>
2025-08-08 18:11:28 -0300

    feat: adding functionality to block users from joining rooms with any random id

--- src/main/java/dev/pubchat/controller/ChatController.java
+++ src/main/java/dev/pubchat/controller/ChatController.java
@@ -1,22 +1,29 @@
 package dev.pubchat.controller;
 
 import dev.pubchat.model.Message;
+import dev.pubchat.repository.RoomRepository;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
 @Controller
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final RoomRepository repository;
 
-    public ChatController(SimpMessagingTemplate messagingTemplate) {
+    public ChatController(SimpMessagingTemplate messagingTemplate,  RoomRepository repository) {
         this.messagingTemplate = messagingTemplate;
+        this.repository = repository;
     }
 
     @MessageMapping("/room/{roomId}")
     public void sendMessage(@DestinationVariable String roomId, Message message) {
+        if (!roomId.equals("global") && !repository.existsRoom(roomId))
+            return;
         String topic = "/topic/room/" + roomId;
         messagingTemplate.convertAndSend(topic, message);
     }
--- src/main/java/dev/pubchat/controller/RoomController.java
+++ src/main/java/dev/pubchat/controller/RoomController.java
@@ -1,8 +1,8 @@
 package dev.pubchat.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.pubchat.repository.RoomRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -10,9 +10,24 @@ import java.util.UUID;
 @RequestMapping("/pubchat")
 public class RoomController {
 
+    private final RoomRepository repository;
+
+    public RoomController(RoomRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping("/rooms")
     public String createRoom() {
-        return UUID.randomUUID().toString();
+        var room = UUID.randomUUID().toString();
+        repository.addRoom(room);
+        return room;
+    }
+
+    @GetMapping("/rooms/{room}/available")
+    public ResponseEntity<Void> existsRoom(@PathVariable String room) {
+        if (repository.existsRoom(room))
+            return ResponseEntity.ok().build();
+        return ResponseEntity.notFound().build();
     }
 
 }
